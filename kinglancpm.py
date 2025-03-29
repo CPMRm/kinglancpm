45bbc291c73f36b41100d975f685e11c7902300c
CPMRm <rmcpmstore@gmail.com>
2025-03-30 07:34:17 +0800

    Update kinglancpm.py

--- kinglancpm.py
+++ kinglancpm.py
@@ -3,7 +3,7 @@ from time import sleep
 
 BASE_URL: str = "https://lan.squareweb.app/api"
 
-class kinglancpm:
+class CPMRm:
     def __init__(self, access_key) -> None:
         self.auth_token = None
         self.access_key = access_key
