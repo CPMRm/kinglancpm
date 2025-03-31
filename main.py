e4f3c4023abd051813bc426ce9cdab99b51098bb
CPMRm <rmcpmstore@gmail.com>
2025-03-31 08:27:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from CPMRm import kinglancpm
+from kinglanscpm import kinglancpm
 
 __CHANNEL_USERNAME__ = "KingLanCPM"
 __GROUP_USERNAME__   = "KingLanCPMchat"
